4cfcbff46e82bdee9614005e591179f832c94827
omochio <kouyoutkg@gmail.com>
2023-02-04 05:29:28 +0900

    根の描画機能を作成

--- Assets/Scripts/RootDrawingController.cs
+++ Assets/Scripts/RootDrawingController.cs
@@ -1,16 +1,15 @@
 ﻿using UnityEngine;
 using UnityEngine.Splines;
 
-public class RootDrawingController : MonoBehaviour
+public class RootDrawintgController : MonoBehaviour
 {
-    // スプライン
     [SerializeField] Transform m_playerTransform;
     [SerializeField] SplineContainer m_splineContainer;
     [SerializeField] float m_knotInterval;
     [SerializeField] Vector3 m_tangentIn;
     [SerializeField] Vector3 m_tangentOut;
 
-    Vector3 m_prevPos = Vector3.zero;
+    Vector3 m_prevPos;
     Spline m_spline;
 
     void Start()
@@ -20,18 +19,21 @@ public class RootDrawingController : MonoBehaviour
         // 全削除
         m_spline.Clear();
 
-        m_spline.Add(new BezierKnot(Vector3.zero, m_tangentIn, m_tangentOut));
+        // 初回knot
+        m_spline.Add(new BezierKnot(transform.position, m_tangentIn, m_tangentOut));
+
+        m_prevPos = transform.position;
     }
 
     void Update()
     {
-        if (m_prevPos == Vector3.zero)
+        // 過去位置と現在地の差がknotIntervalより大きい時
+        if ((m_playerTransform.position - m_prevPos).magnitude > m_knotInterval)
         {
-            m_prevPos = m_playerTransform.position;
-        }
-        else if ((m_playerTransform.position - m_prevPos).magnitude > m_knotInterval)
-        {
+            // Knot追加
             m_spline.Add(new BezierKnot(m_playerTransform.position, m_tangentIn, m_tangentOut));
+
+            // 過去位置更新
             m_prevPos = m_playerTransform.position;
         }
     }
