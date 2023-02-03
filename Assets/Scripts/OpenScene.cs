2e31ea10f7c754944da741732c222094b330f907
GekkouSeki <kohei0175@gmail.com>
2023-02-03 20:42:00 +0900

    AddCommonの修正

--- Assets/Scripts/OpenScene.cs
+++ Assets/Scripts/OpenScene.cs
@@ -5,13 +5,25 @@ namespace Gekkou
 {
     public class OpenScene
     { 
+        
         [MenuItem("Tools/Gekkou/OpenScene/SampleScene")]
         private static void OpenSampleSceneScene()
         {
             // シーンの変更があった場合にどうするか聞く
             if (UnityEditor.SceneManagement.EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo())
             {
-                UnityEditor.SceneManagement.EditorSceneManager.OpenScene("Assets/Commons/SampleScenes/SampleScene.unity");
+                UnityEditor.SceneManagement.EditorSceneManager.OpenScene("Assets/Scenes/SampleScene.unity");
+            }
+        }
+
+
+        [MenuItem("Tools/Gekkou/OpenScene/SampleScene2")]
+        private static void OpenSampleScene2Scene()
+        {
+            // シーンの変更があった場合にどうするか聞く
+            if (UnityEditor.SceneManagement.EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo())
+            {
+                UnityEditor.SceneManagement.EditorSceneManager.OpenScene("Assets/Gekkou/Commons/SampleScenes/SampleScene2.unity");
             }
         }
 
