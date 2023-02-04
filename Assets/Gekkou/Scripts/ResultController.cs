3167a8b08255a4873a9f2e2655f5138087ad8e1f
GekkouSeki <kohei0175@gmail.com>
2023-02-04 09:03:58 +0900

    titleからResultまでの一巡

---
+++ Assets/Gekkou/Scripts/ResultController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Gekkou;
+
+public class ResultController : MonoBehaviour
+{
+    [SerializeField]
+    private Button _retryButton;
+    [SerializeField]
+    private Button _titleButton;
+
+    private void Start()
+    {
+        _retryButton.onClick.AddListener(OnClickRetry);
+        _titleButton.onClick.AddListener(OnClickTitle);
+    }
+
+    public void OnClickRetry()
+    {
+        // game scene に移動
+        SceneSystemManager.Instance.SceneLoading(Scene.GekkouScene);
+    }
+
+    public void OnClickTitle()
+    {
+        // title scene に移動
+        SceneSystemManager.Instance.SceneLoading(Scene.TitleScene);
+    }
+}
---
+++ Assets/Gekkou/Scripts/TitleController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Gekkou;
+
+public class TitleController : MonoBehaviour
+{
+    [SerializeField]
+    private Button _startButton;
+
+    private void Start()
+    {
+        _startButton.onClick.AddListener(OnClickStart);
+    }
+
+    public void OnClickStart()
+    {
+        // game scene に移動
+        SceneSystemManager.Instance.SceneLoading(Scene.GekkouScene);
+    }
+}
