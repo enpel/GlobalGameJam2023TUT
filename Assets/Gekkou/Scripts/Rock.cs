bfabf53b28a76f8626dac95d554111e48ab804ee
GekkouSeki <kohei0175@gmail.com>
2023-02-04 17:58:45 +0900

    背景画像追加

---
+++ Assets/Gekkou/Scripts/Rock.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rock : Nutrition
+{
+    public override bool HitNutritionObject(int penetrationPower)
+    {
+        // 種類が吸収なので吸収
+        if (_nutritionType == NutritionType.Absorption)
+            return true;
+
+        // 抵抗値が上回ったので抵抗
+        if (_resistPenetrationPower > penetrationPower)
+            return false;
+
+        // 下回ったので吸収
+        return true;
+    }
+
+    public override void AbsorbedObject()
+    {
+        Invoke("EnableObj", _disableTime);
+    }
+}
