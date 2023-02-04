047708d0f092c8b834f4e11bf160e53732bb5a8a
GekkouSeki <kohei0175@gmail.com>
2023-02-04 13:26:30 +0900

    optionmanager追加

---
+++ Assets/Gekkou/Scripts/OptionCallButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionCallButton : MonoBehaviour
+{
+    [SerializeField]
+    private Button _callButton;
+
+    private void Start()
+    {
+        if (_callButton == null)
+            _callButton = GetComponent<Button>();
+
+        _callButton.onClick.AddListener(OnOptionCall);
+    }
+
+    public void OnOptionCall()
+    {
+        OptionManager.Instance.EnableOption();
+    }
+}
