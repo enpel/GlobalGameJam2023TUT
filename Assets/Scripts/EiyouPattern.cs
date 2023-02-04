6297011157876d161e5ed8aeea041e7cf350acdd
saltn910 <85836217+saltn910@users.noreply.github.com>
2023-02-04 11:35:34 +0900

    配置パターンのメイン画面適応最適化

--- Assets/Scripts/EiyouPattern.cs
+++ Assets/Scripts/EiyouPattern.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 
 public class EiyouPattern : MonoBehaviour
 {
+    // パターンの枠
     [SerializeField] GameObject pattern1;
     [SerializeField] GameObject pattern2;
+    [SerializeField] GameObject pattern3;
+    [SerializeField] GameObject pattern4;
+    [SerializeField] GameObject pattern5;
 
     void Start()
     {
+        // ランダムにパターンを選出
         int rnd = Random.Range(1, 10);
         Debug.Log(rnd);
         if(rnd <= 5)
