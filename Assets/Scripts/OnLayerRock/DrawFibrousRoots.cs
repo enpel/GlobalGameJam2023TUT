12efc09b31435977eb85a513b839784aa73e6549
omochio <kouyoutkg@gmail.com>
2023-02-05 13:53:14 +0900

    LayerRockのイベントに移動

---
+++ Assets/Scripts/OnLayerRock/DrawFibrousRoots.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class DrawFibrousRoots : LayerRock
+{
+    [SerializeField] FibrousRootsManager m_fib;
+
+    // Update is called once per frame
+    public override void AbsorbedObject()
+    {
+        m_fib.InstantiateFabirousRoots();
+        Invoke("EnableObj", _disableTime); 
+    }
+}
